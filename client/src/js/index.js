a9e5aa2525235bc290e9fcff9bfb898d58c97095
Casey Mcgrath <caseymcgrath1000@gmail.com>
2023-09-04 23:26:42 -0400

    Added install events

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,4 +1,4 @@
-import { Workbox } from 'workbox-window';
+// import { Workbox } from 'workbox-window';
 import Editor from './editor';
 import './database';
 import '../css/style.css';
@@ -26,8 +26,12 @@ if (typeof editor === 'undefined') {
 // Check if service workers are supported
 if ('serviceWorker' in navigator) {
   // register workbox service worker
-  const workboxSW = new Workbox("/service-worker.js");
-  workboxSW.register();
+  // const workboxSW = new Workbox("/service-worker.js");
+  // workboxSW.register();
+
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/service-worker.js");
+  });
 } else {
   console.error('Service workers are not supported in this browser.');
 }
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,11 +2,33 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault();
+    deferredPrompt = event;
+    installBtn.style.visibility = "visible";
+    textHeader.textContent = "Click the button to install!";
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    if (deferredPrompt) {
+        deferredPrompt.prompt();
+        const choiceResult = await deferredPrompt.userChoice;
+        
+        if (choiceResult.outcome === "accepted") {
+        textHeader.textContent = "Successfully installed!";
+        butInstall.style.visibility = "hidden";
+        } else {
+        textHeader.textContent = "Installation declined.";
+        }
+
+    deferredPrompt = null;
+    }
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    textHeader.textContent = "Successfully installed!";
+    console.log("👍", "appinstalled", event);
+});
 
